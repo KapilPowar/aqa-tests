c9dc9e916f6f20ab36e219bc40cc566d7aff79e2
Kapil Powar <Kapil.powar@in.ibm.com>
2023-09-07 16:39:00 +0530

    test

--- jck/jck-semiauto-updater/jckupdater.sh
+++ jck/jck-semiauto-updater/jckupdater.sh
@@ -358,6 +358,9 @@ test() {
 	echo "Response -- $response"
 	pr_number=$(echo "$response" | grep -o '"number": *[0-9]*' | awk -F':' '{print $2}' | tr -d ' ,"')
 
+	TEST_ENV="$pr_number"
+	echo "TEST_ENV== $TEST_ENV"
+	
 	echo "PR -- $pr_number"
 	file_path="$WORKSPACE/../../test/file.txt"
 	mkdir -p "$(dirname "$file_path")"
